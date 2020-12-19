ca006bbe1cd7c069c4e67161e16c27ae5ec65540
Alexander Chursa <tminei@ukr.net>
2020-12-19 20:01:21 +0200

    Add new option: clip_save - save or not translated text to clipboard

--- autotrans2ru.sh
+++ autotrans2ru.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo $path
 config_file=$path/config.json;
-[ -f $config_file ] && copy_mode=$(cat $config_file | jq '.copy_mode') notify=$(cat $config_file | jq '.notify') || echo "$config_file does not exist! Config by default!" copy_mode=false notify=true
+[ -f $config_file ] && copy_mode=$(cat $config_file | jq '.copy_mode') clip_save=$(cat $config_file | jq '.clip_save') notify=$(cat $config_file | jq '.notify') || echo "$config_file does not exist! Config by default!" copy_mode=false notify=true clip_save=true
 $copy_mode && clip=$(xclip -o -selection clipboard) || clip=$(xclip -o)
 tr=$(bash $path/trans -t ru -b "$clip")
 $notify && notify-send "Autotrans" "$tr"
-echo $tr | xclip -sel clip
+$clip_save && echo $tr | xclip -sel clip
